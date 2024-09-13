63c94ca462a00f9e0337133dcf4828c1d1d89ca3
Josh Thor <jthor@42kl.edu.my>
2024-09-14 00:30:51 +0800

    chore: updated answer

--- 1310. XOR Queries of a Subarray/solution.py
+++ 1310. XOR Queries of a Subarray/solution.py
@@ -1,6 +1,6 @@
 def	xorQueries(arr: list[int], queries: list[list[int]]) -> list[int]:
 	# initialise pre-sum array
-	pre_sum = [0]
+	pre_sum = []
 	base = 0
 	for x in arr:
 		base = base ^ x
@@ -10,7 +10,10 @@ def	xorQueries(arr: list[int], queries: list[list[int]]) -> list[int]:
 	# go through queries, and form answer.
 	ans = []
 	for q in queries:
-		ans.append(pre_sum[q[0]] ^ pre_sum[q[1] + 1])
+		if q[0] == 0:
+			ans.append(pre_sum[q[1]])
+		else:
+			ans.append(pre_sum[q[0] - 1] ^ pre_sum[q[1]])
 	return ans
 
 ans = xorQueries([1, 3, 4 ,8], [[0,1],[1,2],[0,3],[3,3]])
