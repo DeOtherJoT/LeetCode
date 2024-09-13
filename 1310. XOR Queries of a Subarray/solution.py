8dc70ea5d3fed1f9357cba8ef1df6146501d3e88
Josh Thor <jthor@42kl.edu.my>
2024-09-14 00:29:24 +0800

    chore: removed comments

--- 1310. XOR Queries of a Subarray/solution.py
+++ 1310. XOR Queries of a Subarray/solution.py
@@ -1,30 +1,3 @@
-# Naive approach - Just brute force it with two for loops. Problem is, Time Limit Exceeded.
-
-# Next approach, we use the identity of a XOR function, that is;
-#	x ^ y ^ x = y.
-
-# Hence,
-# if arr = [a, b, c, d, e]
-#			0, 1, 2, 3, 4
-
-
-# then
-#	query[2, 4] = (c ^ d ^ e)
-#				= (a ^ a) ^ (b ^ b) ^ (c ^ d ^ e)
-#				= (a ^ b) ^ (a ^ b ^ c ^ d ^ e)
-
-# Basically, create an pre-sum array such that
-#	pre-sum = [
-#		0,
-#		a,
-# 		a ^ b,
-#		...
-#		a ^ b ^ c ^ d ^ e
-#]
-
-# Then,
-# query[2,4] == pre-sum[2] ^ pre-sum[5]
-
 def	xorQueries(arr: list[int], queries: list[list[int]]) -> list[int]:
 	# initialise pre-sum array
 	pre_sum = [0]
